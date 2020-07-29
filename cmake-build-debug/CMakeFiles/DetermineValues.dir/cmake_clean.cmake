722806b993dac88e23a2cd32f209773ee20ec456
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2020-07-29 16:55:14 -0700

    Initial Commit of Application

---
+++ cmake-build-debug/CMakeFiles/DetermineValues.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DetermineValues.dir/main.cpp.o"
+  "DetermineValues"
+  "DetermineValues.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DetermineValues.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
